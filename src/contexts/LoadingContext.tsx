22ba8ce8b7996558f9b7877154be773e30a3c0b6
frix4224 <omarfrix@goosetrp.com>
2025-03-18 16:50:02 +0100

    Copy src/contexts/LoadingContext.tsx from ffff

---
+++ src/contexts/LoadingContext.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import Loader from '../components/Loader';
+
+interface LoadingContextType {
+  isLoading: boolean;
+  startLoading: () => void;
+  stopLoading: () => void;
+}
+
+const LoadingContext = createContext<LoadingContextType | undefined>(undefined);
+
+export const LoadingProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const startLoading = () => setIsLoading(true);
+  const stopLoading = () => setIsLoading(false);
+
+  return (
+    <LoadingContext.Provider value={{ isLoading, startLoading, stopLoading }}>
+      <AnimatePresence>
+        {isLoading && <Loader />}
+      </AnimatePresence>
+      {children}
+    </LoadingContext.Provider>
+  );
+};
+
+export const useLoading = () => {
+  const context = useContext(LoadingContext);
+  if (context === undefined) {
+    throw new Error('useLoading must be used within a LoadingProvider');
+  }
+  return context;
+};
